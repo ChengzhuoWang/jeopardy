6b50785e2ea33e627f1d979f4cee2de877397839
Elim T <elimtisgbey@yahoo.com>
2018-01-07 04:12:10 -0500

    changes to dynamic port

--- app/bundle.js
+++ app/bundle.js
@@ -116,7 +116,7 @@ const $ = require('jquery')
 module.exports = {
     getQuestions : function(category, difficulty){
         return $.ajax({
-            url: 'http://localhost:4000/api/v2?category=' + category + '&difficulty=' + difficulty,
+            url: 'http://localhost:'+ process.env.PORT +'/api/v2?category=' + category + '&difficulty=' + difficulty,
             type: 'GET',
             crossDomain: true,
             dataType: 'json',
