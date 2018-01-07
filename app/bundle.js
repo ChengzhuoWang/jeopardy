ae60085002ba3bbb9fc9cb6e786f37a74b6b8d35
Khaled Ali <khaledali@khaleds-Air.hitronhub.home>
2018-01-07 13:42:13 -0700

    Refactored loadQuestion() function and removed global variables

--- app/bundle.js
+++ app/bundle.js
@@ -3,13 +3,6 @@ const dataSource = require('./httpRequests.js');
 
 var score = 0;
 window.onload = function() {
-
-    var modalText = "";
-    var answer = "";
-    var boxOption1="";
-    var boxOption2="";
-    var boxOption3="";
-    var boxOption4="";
     var section = document.querySelector('section');
     section.addEventListener('click',getQuestion);
 }
@@ -20,7 +13,7 @@ function getQuestion() {
     var difficulty = null;
     var questionData = null
     return new Promise((resolve, reject) => {
-        var box = document.getElementById(event.target.id); // store the id of the result in a box variable
+        var box = document.getElementById(event.target.id); // stores the id of the clicked box
         box.style['color'] = 'blue'; //card will be all blue when its been clicked
         id = event.target.id;
         category = document.getElementById(event.target.id).getAttribute('category');
@@ -38,78 +31,54 @@ function getQuestion() {
   // first load the questions and display it
  function loadQuestion(data) {
      var modal = document.getElementById('mod');
-     modalText = document.querySelector('.modal-content p');
-     modalText.innerHTML = ''; // clear modal after every question
+     var modalText = document.querySelector('.modal-content');
+     modalText.textContent = ''; // clear modal after every question
 
      var boxQuestion = document.createElement('p');
-     boxQuestion.innerHTML = data.question;
+     boxQuestion.textContent = data.question;
      boxQuestion.classList.add('question');
-
-     boxOption1 = document.createElement('p');
-     boxOption1.innerHTML = data.options[0];
-     boxOption1.classList.add('option1');
-
-
-     boxOption2 = document.createElement('p');
-     boxOption2.innerHTML = data.options[1];
-     boxOption2.classList.add('option2');
-
-     boxOption3 = document.createElement('p');
-     boxOption3.innerHTML = data.options[2];
-     boxOption3.classList.add('option3');
-
-     boxOption4 = document.createElement('p');
-     boxOption4.innerHTML = data.options[3];
-     boxOption4.classList.add('option4');
-
-
      modalText.appendChild(boxQuestion);
 
-     modalText.appendChild(boxOption1);
-     modalText.appendChild(boxOption2);
-     modalText.appendChild(boxOption3);
-     modalText.appendChild(boxOption4);
+    for (var i = 0; i < 4; i++) {
+        var boxOptions = document.createElement('p');
+        boxOptions.classList.add('option'+i);
+        boxOptions.textContent = data.options[i];
+        modalText.appendChild(boxOptions);
+    }
      modal.style.display = 'block'; // displays modal
 
-     //questionResult(modalText,modal,data); //COMMENT FOR NOW
-    answer = document.createElement('p');
-    answer.innerHTML = data.answer;
+    var answer = document.createElement('p');
+    answer.textContent = data.answer;
     answer.classList.add('answer');
 
-    modalText.addEventListener('click', function(Event){  
-     modalText.innerHTML = '<p class="modal-result">The correct answer is: </p>';
-      modalText.appendChild(answer);
-
-      setTimeout(function(){
-        modal.style.display = 'none';
-      }, 2000); // show the answer then close modal 
-      });
-
-      boxOption1.onclick = function(event){
-      getScore(data);
-      };
-      boxOption2.onclick = function(event){
-      getScore(data);
-      };
-      boxOption3.onclick = function(event){
-      getScore(data);
-      };
-      boxOption4.onclick = function(event){
-      getScore(data);
-      };
-
+    for (var i = 1; i < 5;i++) {
+        modalText.childNodes[i].addEventListener('click', function() {
+          getScore(data, modalText, modal, answer);
+        });
+      }
  }
 
- function getScore(data) {
-  var scoreCheck = document.getElementById('score');
-  if (event.target.innerHTML == answer.innerHTML) {
-        score += data.difficultyLevel;
-      }
-      else {
-        score -= data.difficultyLevel;
-      }
-      scoreCheck.innerHTML = "Player Score: " + score;
- }
+ function getScore(data, modalText, modal, answer) {
+    var scoreCheck = document.getElementById('score');
+    if (event.target.textContent == answer.textContent) {
+          score += data.difficultyLevel;
+        }
+        else {
+          score -= data.difficultyLevel;
+        }
+        scoreCheck.textContent = "Player Score: " + score;
+  
+        displayScore(modalText, modal, answer);
+   }
+  
+   function displayScore(modalText, modal, answer) {
+    modalText.innerHTML = '<p class="modal-result">The correct answer is: </p>';
+    modalText.appendChild(answer);
+    
+    setTimeout(function(){
+      modal.style.display = 'none';
+    }, 2000); // show the answer then close modal after
+   }
 },{"./httpRequests.js":2}],2:[function(require,module,exports){
 const $ = require('jquery')
 
