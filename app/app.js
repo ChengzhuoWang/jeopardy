67f8028e3d1c3ff7af93f7784e84064ee3f71ab8
Khaled Ali <khaledali@khaleds-Air.hitronhub.home>
2018-01-08 19:36:17 -0700

    refactored to es6

--- app/app.js
+++ app/app.js
@@ -1,80 +1,78 @@
 const dataSource = require('./httpRequests.js');
 
-var score = 0;
-window.onload = function() {
-    var section = document.querySelector('section');
-    section.addEventListener('click',getQuestion);
+let score = 0;
+window.onload = () => {
+const section = document.querySelector('section');
+  section.addEventListener('click',getQuestion);
 }
 
 function getQuestion() {
-    var id = null;
-    var category = null;
-    var difficulty = null;
-    var questionData = null
-    return new Promise((resolve, reject) => {
-        var box = document.getElementById(event.target.id); // stores the id of the clicked box
-        box.style['color'] = 'blue'; //card will be all blue when its been clicked
-        id = event.target.id;
-        category = document.getElementById(event.target.id).getAttribute('category');
-        difficulty = document.getElementById(event.target.id).getAttribute('difficulty');
-        setTimeout(function(){
-            resolve("Success!");
-        }, 250);
-    }).then(() => {
-        return dataSource.getQuestions(category, difficulty) // get questions based on category and difficulty
-    }).then((data) => {
-        loadQuestion(data[0]); // select the first question. Randomize question selection in the future
-    })
+  let id = null;
+  let category = null;
+  let difficulty = null;
+  let questionData = null;
+  return new Promise((resolve, reject) => {
+    let box = document.getElementById(event.target.id); // stores the id of the clicked box
+    box.style['color'] = 'blue'; //card will be all blue when its been clicked
+
+    id = event.target.id;
+    category = document.getElementById(event.target.id).getAttribute('category');
+    difficulty = document.getElementById(event.target.id).getAttribute('difficulty');
+    setTimeout(function(){
+      resolve("Success!");
+    }, 250);
+  }).then(() => {
+    return dataSource.getQuestions(category, difficulty) // get questions based on category and difficulty
+  }).then((data) => {
+    loadQuestion(data[0]); // select the first question. Randomize question selection in the future
+  })
 }
 
   // first load the questions and display it
  function loadQuestion(data) {
-     var modal = document.getElementById('mod');
-     var modalText = document.querySelector('.modal-content');
-     modalText.textContent = ''; // clear modal after every question
+  const modal = document.querySelector('#modal');
+  let modalText = document.querySelector('.modal-content');
+  modalText.textContent = ''; // clear modal after every question
 
-     var boxQuestion = document.createElement('p');
-     boxQuestion.textContent = data.question;
-     boxQuestion.classList.add('question');
-     modalText.appendChild(boxQuestion);
+  const boxQuestion = document.createElement('p');
+  boxQuestion.textContent = data.question;
+  boxQuestion.classList.add('question');
+  modalText.appendChild(boxQuestion);
 
-    for (var i = 0; i < 4; i++) {
-        var boxOptions = document.createElement('p');
-        boxOptions.classList.add('option'+i);
-        boxOptions.textContent = data.options[i];
-        modalText.appendChild(boxOptions);
-    }
-     modal.style.display = 'block'; // displays modal
+  for (let i = 0; i < 4; i++) {
+    let boxOptions = document.createElement('p');
+    boxOptions.classList.add('option'+i);
+    boxOptions.textContent = data.options[i];
+    modalText.appendChild(boxOptions);
+  }
+  modal.style.display = 'block'; // displays modal
 
-    var answer = document.createElement('p');
-    answer.textContent = data.answer;
-    answer.classList.add('answer');
+  let answer = document.createElement('p');
+  answer.textContent = data.answer;
+  answer.classList.add('answer');
 
-    for (var i = 1; i < 5;i++) {
-        modalText.childNodes[i].addEventListener('click', function() {
-          getScore(data, modalText, modal, answer);
-        });
-      }
+  for (let i = 1; i < 5;i++) {
+    modalText.childNodes[i].addEventListener('click', () => {
+    getScore(data, modalText, modal, answer);
+    });
+  }
  }
 
- function getScore(data, modalText, modal, answer) {
-    var scoreCheck = document.getElementById('score');
-    if (event.target.textContent == answer.textContent) {
-          score += data.difficultyLevel;
-        }
-        else {
-          score -= data.difficultyLevel;
-        }
-        scoreCheck.textContent = "Player Score: " + score;
+function getScore(data, modalText, modal, answer) {
+  let scoreCheck = document.getElementById('score');
+  (event.target.textContent == answer.textContent) ? 
+  score+= data.difficultyLevel :
+  score-= data.difficultyLevel;
+
+  scoreCheck.textContent = "Player Score: " + score;
+  displayScore(modalText, modal, answer);
+}
   
-        displayScore(modalText, modal, answer);
-   }
-  
-   function displayScore(modalText, modal, answer) {
-    modalText.innerHTML = '<p class="modal-result">The correct answer is: </p>';
-    modalText.appendChild(answer);
+function displayScore(modalText, modal, answer) {
+  modalText.innerHTML = '<p class="modal-result">The correct answer is: </p>';
+  modalText.appendChild(answer);
     
-    setTimeout(function(){
-      modal.style.display = 'none';
-    }, 2000); // show the answer then close modal after
-   }
+  setTimeout(() => {
+    modal.style.display = 'none';
+  }, 2000); // show the answer then close modal after
+}
